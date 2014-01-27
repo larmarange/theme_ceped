2aa29b0b63c6d95e5a6f540591ae466b86669b8d
Joseph <joseph@larmarange.net>
2014-01-27 19:07:28 +0200

    On part du thème Larma https://github.com/larmarange/larma

---
+++ lang/theme_larma_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/menus/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'afficher_menu' => 'Afficher / masquer le menu',
+	'retour_haut_page' => 'Retour haut de page'
+	
+);
+
+?>
